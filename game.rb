15f4b446f30ade48534595bd6d5c111a6d168fac
David Becerra <DavidBecerra84@gmail.com>
2014-06-10 17:37:21 -0700

    Initial commit

---
+++ game.rb
@@ -0,0 +1,36 @@
+require_relative 'player'
+require_relative 'die'
+
+class Game
+  attr_reader :title
+
+  def initialize(title)
+    @title = title
+    @players = []
+  end
+
+  def add_player(player)
+    @players << player
+  end
+
+  def play
+    puts "There are #{@players.size} players in the game:"
+    @players.each do |player|
+      puts player
+    end
+
+    @players.each do |player|
+      die = Die.new
+      number_rolled = die.roll
+
+      case number_rolled
+      when 1..2
+        player.blam
+      when 3..4
+        puts "#{player.name} was skipped"
+      else
+        player.w00t
+      end
+    end
+  end
+end

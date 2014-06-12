a9c1d217779e458be6af8e4159cf1bb037b4123e
David Becerra <DavidBecerra84@gmail.com>
2014-06-12 00:11:49 -0700

    manage rounds with user input

--- studio_game.rb
+++ studio_game.rb
@@ -9,11 +9,26 @@ muppets.add_player(player1)
 muppets.add_player(player2)
 muppets.add_player(player3)
 
-# play up to a 2000 point total
-muppets.play(10) do
-  muppets.total_points >= 2000
+loop do
+  puts "\nHow many rounds do you want to play? ('quit' to exit)"
+  answer = gets.chomp.downcase
+
+  case answer
+  when /^\d+$/
+    muppets.play(answer.to_i)
+  when 'quit', 'exit'
+    muppets.print_stats
+    break
+  else
+    puts "\nPlease enter a number or type 'quit'"
+  end
 end
 
-muppets.print_stats
+# play up to a 2000 point total
+#muppets.play(10) do
+  #muppets.total_points >= 2000
+#end
+
+#muppets.print_stats
 
 

1ecdc7ed8a5301f4f6b645bdb8cc563ce4fbb3e1
David Becerra <DavidBecerra84@gmail.com>
2014-06-12 10:27:40 -0700

    update stub to use 'expect' syntax

--- game_spec.rb
+++ game_spec.rb
@@ -13,21 +13,21 @@ describe Game do
   end
 
   it "w00ts the player if a high number is rolled" do
-    Die.any_instance.stub(:roll).and_return(5)
+    allow_any_instance_of(Die).to receive(:roll).and_return(5)
     @game.play(2)
 
     expect(@player.health).to eq(@initial_health + (15 * 2))
   end
 
   it "skips the player if a medium number is rolled" do
-    Die.any_instance.stub(:roll).and_return(3)
+    allow_any_instance_of(Die).to receive(:roll).and_return(3)
     @game.play(2)
 
     expect(@player.health).to eq @initial_health
   end
 
   it 'blams the player if a low number is rolled' do
-    Die.any_instance.stub(:roll).and_return(1)
+    allow_any_instance_of(Die).to receive(:roll).and_return(1)
     @game.play(2)
 
     expect(@player.health).to eq @initial_health - (10 * 2)
